3b9cd4351597c7363be020ddbc25ef86ac6a3422
Santiago1902200 <santiagocd00@gmail.com>
2025-04-19 23:23:45 +0000

    v1 Inicio-registro sesion

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function mostrarOcultarContrasena() {
+  const inputContrasena = document.getElementById("contrasena");
+  const icono = document.getElementById("iconoOjo");
+
+  const esOculta = inputContrasena.type === "password";
+  inputContrasena.type = esOculta ? "text" : "password";
+  icono.className = esOculta ? "bi bi-eye-slash" : "bi bi-eye";
+}
